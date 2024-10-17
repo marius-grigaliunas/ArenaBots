1a7f8f5da9ae62dc93def5bc3a6b4f2e55ccbfeb
Marius Grigaliūnas <cia.marius.grigaliunas@gmail.com>
2024-10-17 04:46:34 +0200

    Started to implement The Robot Editor

--- Code/PlayerMovement.cs
+++ Code/PlayerMovement.cs
@@ -6,25 +6,10 @@ public partial class PlayerMovement : CharacterBody3D
     [Export] float movementSpeed = 5f;
     Vector3 velocity;
     [Export] CsgBox3D mesh;
-    private bool isCollidingThisFrame;
-    private bool wasCollidingLastFrame;
-    StandardMaterial3D mat1 = new StandardMaterial3D();
-
 
     public override void _PhysicsProcess(double delta)
     {
-        isCollidingThisFrame = CheckCollisionWithWall();
 
-        if (isCollidingThisFrame && !wasCollidingLastFrame)
-        {
-            //Start
-            StartCollisionWithWall();
-        }
-
-        if (!isCollidingThisFrame && wasCollidingLastFrame)
-        {
-            EndCollisionWithWall();
-        }
 
         velocity = Vector3.Zero;
 
@@ -55,36 +40,6 @@ public partial class PlayerMovement : CharacterBody3D
 
         MoveAndSlide();
 
-        wasCollidingLastFrame = isCollidingThisFrame;
     }
 
-    public void StartCollisionWithWall()
-    {
-        mat1.AlbedoColor = Color.FromString("Red", Colors.Red);
-
-        GD.Print("Collision started!");
-        mesh.MaterialOverride = mat1;
-    }
-
-    public void EndCollisionWithWall()
-    {
-        GD.Print("Collision Ended!");
-        mesh.MaterialOverride = null;
-    }
-
-    public bool CheckCollisionWithWall()
-    {
-        for (int i = 0; i < GetSlideCollisionCount(); i++)
-        {
-            var collision = GetSlideCollision(i);
-
-            if (((Node)collision.GetCollider()).Name.ToString().StartsWith("Obstacle"))
-            {
-
-                return true;
-            }
-        }
-
-        return false;
-    }
 }
--- Code/World.cs
+++ Code/World.cs
@@ -1,16 +1,25 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class World : Node
 {
+	public List<Shape3D> baseChassisOptions = new List<Shape3D>();
+
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		BoxShape3D box = new BoxShape3D();
+		SphereShape3D sphere = new SphereShape3D();
 		
+		baseChassisOptions.Add(box);
+		baseChassisOptions.Add(sphere);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+
 	}
 }
