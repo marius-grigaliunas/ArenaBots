728acfdd23fab85ea86e0382bbb51d57a19280a8
Marius Grigaliūnas <cia.marius.grigaliunas@gmail.com>
2024-10-20 00:57:02 +0200

    baseChasis selection logic, exception if it's more than once

--- Code/World.cs
+++ Code/World.cs
@@ -4,22 +4,41 @@ using System.Collections.Generic;
 
 public partial class World : Node
 {
-	public List<Shape3D> baseChassisOptions = new List<Shape3D>();
-
+	public List<MeshInstance3D> baseChassisOptions = new List<MeshInstance3D>();
+	[Export] Node3D bot = new Node3D();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		BoxShape3D box = new BoxShape3D();
-		SphereShape3D sphere = new SphereShape3D();
-		
-		baseChassisOptions.Add(box);
-		baseChassisOptions.Add(sphere);
+		MeshInstance3D boxMesh = new MeshInstance3D();
+		MeshInstance3D capsuleMesh = new MeshInstance3D();
+
+		BoxMesh box = new BoxMesh();
+		boxMesh.Mesh = box;
+
+        CapsuleMesh capsule = new CapsuleMesh();
+        capsuleMesh.Mesh = capsule;
+
+        baseChassisOptions.Add(boxMesh);
+		baseChassisOptions.Add(capsuleMesh);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 
+	}
+
+    public void OnBaseOptionSelected(int index)
+	{
+        for (int i = 0; i < bot.GetChildren().Count; i++)
+		{
+			bot.GetChild(i).QueueFree();
+		}
+
+        bot.AddChild(baseChassisOptions[index]);
+		
+		GD.Print("Added " + baseChassisOptions[index].Name);
+		GD.Print(index);
 	}
 }
